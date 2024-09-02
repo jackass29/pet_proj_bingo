2e5ed443e338aeaff584dcb6f2597bc31df18118
Nazarii Kwasnytsia <somemail@domain.com>
2024-09-03 00:14:08 +0300

    hcafsd

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from fasthtml import common as ft
 app, rt = ft.fast_app()
 
 @rt('/')
-def get(): return ft.Div(P('Hello World!'), hx_get="/change")
+def get(): return ft.Div(ft.P('Hello World!'), hx_get="/change")
 
 ft.serve()
